69e16bafb24b03791a20265ef03e3d84ee9e07c3
sidglas <sidglas@hotmail.com>
2020-03-08 21:42:28 -0300

    getting dollar cotation from Brazil Central Bank - Bacen

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const port = process.env.PORT || 3000
 
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require( './lib/api.bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    console.log('cotação ', cotacao)
+    res.render('home' , {
+        cotacao
+    })
 })
 app.get('/cotacao', (req, res) => {
     console.log (req.query)
